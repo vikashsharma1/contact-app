34cd2a6528845246126317d7e0c49b4a48ab79d4
shilpapandey1 <shilpa.pandey09@gmail.com>
2022-08-16 23:14:25 +0530

    server-api

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import api from "../api/contacts";
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList/ContactList';
@@ -9,6 +10,13 @@ import ContactDetail from './ContactDetail';
 
 function App() {
   const [contacts, setContacts] = useState([]);
+
+  // retreiveContacts
+  const retreiveContacts= async()=> {
+    const response=await api.get("/contacts");
+    
+    return response.data;
+  };
  
   const addContactHandler = (contact) => {
     setContacts([...contacts, contact]);
@@ -22,15 +30,19 @@ function App() {
   }
 
   useEffect(() => {
-    const retrieveContacts = JSON.parse(localStorage.getItem("contacts"))
-    if (retrieveContacts) {
-      setContacts(retrieveContacts);
-    }
+    // const retrieveContacts = JSON.parse(localStorage.getItem("contacts"))
+    // if (retrieveContacts) {
+    //   setContacts(retrieveContacts);
+    const getAllContacts= async() => {
+     const allContacts= await retreiveContacts();
+     if(allContacts) setContacts(allContacts);
+    };
+    getAllContacts();
     }, []);
 
   useEffect(() => {
-     if (contacts && contacts.length>0) {
-      localStorage.setItem("contacts", JSON.stringify(contacts));
+      if (contacts && contacts.length>0) {
+    //   localStorage.setItem("contacts", JSON.stringify(contacts));
      }
 
   },[contacts]);
